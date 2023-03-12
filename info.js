dbbcb8ba160a0a8f61d3357eb9524e2c6f95f255
Kübra Sert <elmastas571@gmail.com>
2023-03-12 11:01:43 +0300

    Create a Guess Number GAME

---
+++ info.js
@@ -0,0 +1,22 @@
+var t=Math.floor((Math.random()*100)+1);
+var count=0;
+console.log(t);
+function Kontrol(){
+    var i=Number(document.getElementById("in1").value);
+
+    if(t>i){
+    count++;
+    document.getElementById("cc").innerHTML=count;
+    document.getElementById("sonuc").innerHTML="Please Enter Big Number Than Your Guess Number";
+    }
+    else if(t<i){
+        count++;
+        document.getElementById("cc").innerHTML=count;
+        document.getElementById("sonuc").innerHTML="Please Enter Small Number Than Your Guess Number";
+    }
+    else if(t==i){
+    count++;
+    document.getElementById("cc").innerHTML=count;
+    document.getElementById("sonuc").innerHTML="Congrilations You Guess the True Number";
+    }
+}
